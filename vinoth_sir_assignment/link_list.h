41c952219569365c78f6360411629af1864fe8d3
Navinagaraj <navinagaraj18@gmail.com>
2019-08-01 11:37:34 +0530

    /* Link_list*/

---
+++ vinoth_sir_assignment/link_list.h
@@ -0,0 +1,26 @@
+/**********************************************/
+/* Author	: Naveenkumar N               */
+/* Date		: 1-08-2019                   */
+/* Filename	: link_list.c                 */
+/* Description	: Linklist Insert delete Node */
+/**********************************************/
+
+#ifndef BASIC_INFO_H
+#define BASIC_INFO_H
+
+
+/* Included header files */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+ 
+/* Macro Definitions */
+
+
+/* function prototypes */
+
+
+/* user defined datatype definitions */
+
+#endif //BASIC_INFO_H
