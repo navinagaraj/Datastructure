72f88adab21f84784e4bd4b6937b13189824399e
navinagaraj <navinagaraj18@gmail.com>
2020-05-19 21:45:55 +0530

    queue

---
+++ queue/queue.h
@@ -0,0 +1,30 @@
+/**********************************************/
+/* Author       : Naveenkumar N               */
+/* Date         : 0-00-0000                   */
+/* Filename     :                             */
+/* Description  :                             */
+/**********************************************/
+
+
+#ifndef QUEUE_H
+#define QUEUE_H
+
+/* Included header files */
+
+#include <stdio.h>
+
+
+
+/* Macro Definitions */
+#define SIZE 5
+
+int items[SIZE], front = -1, rear = -1;
+/* Functions prototype */
+void enQueue(int);
+void deQueue();
+void display();
+
+
+/* user defined datatype definitions */
+
+#endif //QUEUE_H
